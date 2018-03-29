08457aec3473da81e5afa8bff723d1cdeed95aef
root <root@vivek>
2018-03-29 10:38:14 +0000

    changed return types of my_strlen to size_t

--- string.c
+++ string.c
@@ -22,6 +22,7 @@ int main(void)
    for (i = 0; i < 2; ++i)
       
    printf("string lenght of %s = %d\n", s[i], my_strlen(s[i]));
+   printf("string lenght of %s = %lu\n", s[i], my_strlen(s[i]));
 
    return 0;
 }
